570cbe19d2b1b4c219bcb53dce3141de624133de
Pau Gómez Kabelka <Pau@macPau.home>
2020-04-24 18:19:46 +0200

    update

--- RUNME.sh
+++ RUNME.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 scrapy crawl habitacliaRent
 scrapy crawl habitacliaBuy
+jupyter nbconvert   --execute visualisationRent.ipynb --inplace 
+jupyter nbconvert   --execute visualisationBuy.ipynb --inplace 
+git pull
+git stage *
+git commit -m "update"
+git push
